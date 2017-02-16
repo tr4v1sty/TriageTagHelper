f9cbb9e0dae66b40cece49801e90aff45973f5d1
Travis Thompson <travist@fb.com>
2017-02-15 20:48:54 -0800

    added8 tag buttons

--- TriageTagHelper/src/sample/Controller.java
+++ TriageTagHelper/src/sample/Controller.java
@@ -15,6 +15,24 @@ public class Controller {
     @FXML
     Button idmButton;
     @FXML
+    Button deploymentButton;
+    @FXML
+    Button tableauButton;
+    @FXML
+    Button reverseLogistics;
+    @FXML
+    Button gstButton;
+
+    @FXML
+    Button corpInfraOpsButton;
+    @FXML
+    Button salesAppsButton;
+    @FXML
+    Button itAppsButton;
+    @FXML
+    Button avvcButton;
+
+    @FXML
     Label tagLabel;
 
     public void handlechcHDbutton(){
@@ -31,6 +49,13 @@ public class Controller {
         tagLabel.setText("Identity Management, Identity Management - XXXXX");
     }
 
+    public void handledeploymentButton(){tagLabel.setText("deployment, itlogistics, and the building tag");}
+    public void handletableauButton(){tagLabel.setText("nexus, nxs-access or nxs-support");}
+    public void handlereverseLogisticsButton(){tagLabel.setText("reverse logistics, itlogistics");}
+    public void handlegstButton(){tagLabel.setText("GST, print support");}
 
-
+    public void handleCorpInfraOpsButton(){tagLabel.setText("CorpInfraOps");}
+    public void handlesalesAppsButton(){tagLabel.setText("salesapps");}
+    public void handleitAppsButton(){tagLabel.setText("itapps");}
+    public void handleavvcButton(){tagLabel.setText("AV-VC");}
 }
